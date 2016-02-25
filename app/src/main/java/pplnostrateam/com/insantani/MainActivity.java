be7771a44b8fe375b1818446aac4d614f85921bf
adrikacang <adrianseptiandry@gmail.com>
2016-02-25 15:41:03 +0700

    Ganti isi tex didalem mTextView jadi insantari aja

--- app/src/main/java/pplnostrateam/com/insantani/MainActivity.java
+++ app/src/main/java/pplnostrateam/com/insantani/MainActivity.java
@@ -2,12 +2,20 @@ package pplnostrateam.com.insantani;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.TitleTextView);
+
+        mTextView.setText("Insantani aja");
     }
 }
