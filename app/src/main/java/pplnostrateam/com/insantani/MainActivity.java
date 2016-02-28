9690619f98f8ce7dbc506fc67fb923aacaffe598
Adrian <adrianseptiandry@gmail.com>
2016-02-28 23:40:43 +0700

    Benerin compile error

--- app/src/main/java/pplnostrateam/com/insantani/MainActivity.java
+++ app/src/main/java/pplnostrateam/com/insantani/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.TitleTextView);
-        mTextView.setText("Insantani aja");
+        mTextView = (TextView) findViewById(R.id.VegetableText);
+        mTextView.setText("Insantani");
     }
 }
