24c7597739f3fbc43e7f6c0475fc3b70e3a46c87
A. Arisya-Putra <aghny.arisya@gmail.com>
2016-03-15 10:27:03 +0700

    connect login to main activity

---
+++ app/src/main/java/pplnostrateam/com/insantani/Model/Login.java
@@ -0,0 +1,61 @@
+package pplnostrateam.com.insantani.Model;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import pplnostrateam.com.insantani.R;
+import pplnostrateam.com.insantani.UI.MainActivity;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    EditText etUsername, etPassword;
+    TextView tvRegisterLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(this);
+        tvRegisterLink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+            case R.id.tvRegisterLink:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/pplnostrateam/com/insantani/Model/Register.java
@@ -0,0 +1,54 @@
+package pplnostrateam.com.insantani.Model;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import pplnostrateam.com.insantani.R;
+import pplnostrateam.com.insantani.UI.MainActivity;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etName, etPhone, etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        etName = (EditText) findViewById(R.id.etName);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bRegister:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+}
